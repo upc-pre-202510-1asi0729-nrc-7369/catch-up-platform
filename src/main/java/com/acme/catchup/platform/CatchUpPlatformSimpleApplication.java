a8674719047557865d94101aa7bf2513cce4d76a
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2025-10-19 11:40:56 -0500

    chore:initial setup

---
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.acme.catchup.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchUpPlatformSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchUpPlatformSimpleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/acme/catchup/platform/shared/infraestructure/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -0,0 +1,32 @@
+package com.acme.catchup.platform.shared.infraestructure.persistence.jpa.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy {
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+}
