f7f109dc926c2f4d7acd518d124b95a911aab58e
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2025-10-23 17:23:38 -0500

    chore:update settings

--- src/main/java/com/acme/catchup/platform/CatchUpPlatformSimpleApplication.java
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformSimpleApplication.java
@@ -2,7 +2,10 @@ package com.acme.catchup.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CatchUpPlatformSimpleApplication {
 
